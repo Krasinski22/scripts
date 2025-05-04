bd60b0ec631de42706f39b5dee17086b42391667
Your Name <you@example.com>
2025-05-04 18:39:10 -0300

    a

--- Winget-Installs.ps1
+++ Winget-Installs.ps1
@@ -5,26 +5,27 @@ if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 }
 
 winget install 7zip
-winget install AutoHotkey.AutoHotkey
+# winget install AutoHotkey.AutoHotkey
 winget install Cloudflare.Warp
-winget install Discord.Discord
+# winget install Discord.Discord
 winget install File-New-Project.EarTrumpet
 winget install git.git
 winget install glzr-io.glazewm
 winget install KeePassXCTeam.KeePassXC
 winget install CrystalRich.LockHunter
-winget install Neovim.Neovim
+# winget install Neovim.Neovim
 winget install Microsoft.PowerShell
-winget install AntoineAflalo.SoundSwitch
+# winget install AntoineAflalo.SoundSwitch
 winget install Valve.Steam
 winget install SublimeHQ.SublimeText.4
 winget install Telegram.TelegramDesktop
 winget install AntibodySoftware.WizTree
 winget install AmN.yasb
-winget install Armin2208.WindowsAutoNightMode
-winget install Python.Python.3.13
+# winget install Armin2208.WindowsAutoNightMode
+# winget install Python.Python.3.13
 winget install yt-dlp.yt-dlp
-winget install Syncthing.Syncthing 
+# winget install Syncthing.Syncthing 
+winget install Martchus.syncthingtray
 winget install Gyan.FFmpeg 
 winget install GNU.Nano 
 winget install JanDeDobbeleer.OhMyPosh 
@@ -32,5 +33,5 @@ winget install Google.Chrome
 winget install gerardog.gsudo 
 winget install voidtools.Everything
 winget install gnu.wget2
-winget install voidtools.Everything.Alpha
+# winget install voidtools.Everything.Alpha
 winget install CrystalDewWorld.CrystalDiskInfo.KureiKeiEdition
