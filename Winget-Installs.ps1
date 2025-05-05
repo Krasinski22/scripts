550018ebcaa94253b69ac268d3308a3d7245d019
Your Name <you@example.com>
2025-05-05 14:53:35 -0300

    a

--- Winget-Installs.ps1
+++ Winget-Installs.ps1
@@ -4,34 +4,39 @@ if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
     exit
 }
 
-winget install 7zip
+$apps = @(
+    "7zip",
+    "Cloudflare.Warp",
+    "File-New-Project.EarTrumpet",
+    "git.git",
+    "glzr-io.glazewm",
+    "KeePassXCTeam.KeePassXC",
+    "CrystalRich.LockHunter",
+    "Neovim.Neovim",
+    "Microsoft.PowerShell",
+    "Valve.Steam",
+    "SublimeHQ.SublimeText.4",
+    "Telegram.TelegramDesktop",
+    "AntibodySoftware.WizTree",
+    "AmN.yasb",
+    "yt-dlp.yt-dlp",
+    "Martchus.syncthingtray",
+    "Gyan.FFmpeg",
+    "GNU.Nano",
+    "JanDeDobbeleer.OhMyPosh",
+    "Google.Chrome",
+    "gerardog.gsudo",
+    "voidtools.Everything",
+    "gnu.wget2",
+    "CrystalDewWorld.CrystalDiskInfo.KureiKeiEdition"
+)
+
+foreach ($app in $apps) {
+    winget install --id $app --silent
+}
+
 # winget install AutoHotkey.AutoHotkey
-winget install Cloudflare.Warp
 # winget install Discord.Discord
-winget install File-New-Project.EarTrumpet
-winget install git.git
-winget install glzr-io.glazewm
-winget install KeePassXCTeam.KeePassXC
-winget install CrystalRich.LockHunter
-winget install Neovim.Neovim
-winget install Microsoft.PowerShell
 # winget install AntoineAflalo.SoundSwitch
-winget install Valve.Steam
-winget install SublimeHQ.SublimeText.4
-winget install Telegram.TelegramDesktop
-winget install AntibodySoftware.WizTree
-winget install AmN.yasb
 # winget install Armin2208.WindowsAutoNightMode
-# winget install Python.Python.3.13
-winget install yt-dlp.yt-dlp
-# winget install Syncthing.Syncthing 
-winget install Martchus.syncthingtray
-winget install Gyan.FFmpeg 
-winget install GNU.Nano 
-winget install JanDeDobbeleer.OhMyPosh 
-winget install Google.Chrome 
-winget install gerardog.gsudo 
-winget install voidtools.Everything
-winget install gnu.wget2
 # winget install voidtools.Everything.Alpha
-winget install CrystalDewWorld.CrystalDiskInfo.KureiKeiEdition
