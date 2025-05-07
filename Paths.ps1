967d7d6b9fdf609390b556a018b9dedd30f5d4c2
Your Name <you@example.com>
2025-05-07 17:05:05 -0300

    a

--- Paths.ps1
+++ Paths.ps1
@@ -1,17 +1,5 @@
-function Add-To-Path {
-    param(
-        [string]$novoCaminho
-    )
-
-    # Verifica se o caminho já está na PATH
-    if ($env:PATH -notcontains $novoCaminho) {
-        # Adiciona o caminho à PATH do usuário
-        [System.Environment]::SetEnvironmentVariable("PATH", "$env:PATH;$novoCaminho", [System.EnvironmentVariableTarget]::User)
-        Write-Host "Caminho '$novoCaminho' adicionado à PATH."
-    } else {
-        Write-Host "Caminho '$novoCaminho' já está na PATH."
-    }
-
-}
-
-Add-To-Path "C:\Program Files\Sublime Text\"
+"C:\Program Files\Sublime Text\"
+"D:\teste1"
+"D:\teste2"
+"D:\teste3"
+"D:\teste4"
