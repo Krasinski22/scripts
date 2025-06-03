97924e40a966d50cf5bbaebe4adba3bd91cfe683
Your Name <you@example.com>
2025-06-03 16:32:14 -0300

    b

--- linux-links.sh
+++ linux-links.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-ln -sf "$HOME"/scripts/polybar "$HOME"/.config/polybar
+rm -rf "$HOME/.config/polybar"
+ln -sf "$HOME/scripts/polybar" "$HOME/.config/polybar"
 
-ln -sf "$HOME"/scripts/linux/i3 "$HOME"/.config//i3
+rm -rf "$HOME/.config/i3"
+ln -sf "$HOME/scripts/i3" "$HOME/.config/i3"
 
-ln -sf "$HOME"/scripts/mpv "$HOME"/.config/mpv
+rm -rf "$HOME/.config/mpv"
+ln -sf "$HOME/scripts/mpv" "$HOME/.config/mpv"
 
-ln -sf "$HOME"/scripts/linux/fish "$HOME"/.config/fish
+rm -rf "$HOME/.config/fish"
+ln -sf "$HOME/scripts/fish" "$HOME/.config/fish"
 
-ln -sf "$HOME"/scripts/linux/polybar "$HOME"/.config/polybar
+rm -rf "$HOME/.config/keepassxc"
+ln -sf "$HOME/scripts/keepassxc" "$HOME/.config/keepassxc"
 
-ln -sf "$HOME"/scripts/linux/keepassxc "$HOME"/.config/keepassxc
+rm -rf "$HOME/.config/kitty"
+ln -sf "$HOME/scripts/kitty" "$HOME/.config/kitty"
 
-ln -sf "$HOME"/scripts/linux/kitty "$HOME"/.config/kitty
+rm -rf "$HOME/.config/sublime-text/Packages/User"
+mkdir -p "$HOME/.config/sublime-text/Packages"
+ln -sf "$HOME/scripts/sublime/User" "$HOME/.config/sublime-text/Packages/User"
 
-ln -sf "$HOME"/scripts/sublime/User "$HOME"/.config/sublime-text/Packages/User
+rm -rf "$HOME/.config/i3status"
+ln -sf "$HOME/scripts/i3status" "$HOME/.config/i3status"
 
+rm -rf "$HOME/.config/i3blocks"
+ln -sf "$HOME/scripts/i3blocks" "$HOME/.config/i3blocks"
