7423090fccafb25d267e0a94196b41a48e6e970b
Your Name <you@example.com>
2025-06-03 15:42:23 -0300

    b

--- linux-links.sh
+++ linux-links.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 ln -sf "$HOME"/scripts/polybar "$HOME"/.config/polybar
-ln -sf "$HOME"/scripts/i3 "$HOME"/.config/i3
+
+ln -sf "$HOME"/scripts/linux/i3 "$HOME"/.config//i3
+
 ln -sf "$HOME"/scripts/mpv "$HOME"/.config/mpv
+
+ln -sf "$HOME"/scripts/linux/fish "$HOME"/.config/fish
+
+ln -sf "$HOME"/scripts/linux/polybar "$HOME"/.config/polybar
+
+ln -sf "$HOME"/scripts/linux/keepassxc "$HOME"/.config/keepassxc
+
+ln -sf "$HOME"/scripts/linux/kitty "$HOME"/.config/kitty
+
+ln -sf "$HOME"/scripts/sublime/User "$HOME"/.config/sublime-text/Packages/User
 
