ec88a3492bbe62097804876be0f54f989bf27387
Your Name <you@example.com>
2025-06-05 09:58:41 -0300

    m

---
+++ linux-installs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y \
+git \
+fish \
+brave-browser \
+cloudflare-warp \
+i3 \
+kitty \
+mpv \
+picom \
+polybar \
+rclone \
+rofi \
+sublime-text \
+syncthingtray \
+xdottool \
+zsh \
+steam
