85bae3e162c8a290e1f0f533ae918bac645afe13
a <b@a.com>
2025-06-06 12:40:12 -0300

    a

--- linux-links.sh
+++ linux-links.sh
@@ -1,29 +1,54 @@
 #!/bin/bash
 
-rm -rf "$HOME/.config/polybar"
-ln -sf "$HOME/scripts/polybar" "$HOME/.config/polybar"
+# Verifica se o script está sendo executado como root (UID 0)
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Este script deve ser executado como root ou usando 'sudo'."
+    exit 1
+fi
 
-rm -rf "$HOME/.config/i3"
-ln -sf "$HOME/scripts/i3" "$HOME/.config/i3"
+# Define a variável USER_HOME para ser o diretório home do usuário que invocou o sudo
+USER_HOME=$(eval echo "~$SUDO_USER")
 
-rm -rf "$HOME/.config/mpv"
-ln -sf "$HOME/scripts/mpv" "$HOME/.config/mpv"
+rm -rf "$USER_HOME/.config/polybar"
+ln -sf "$USER_HOME/scripts/polybar" "$USER_HOME/.config/polybar"
 
-rm -rf "$HOME/.config/fish"
-ln -sf "$HOME/scripts/fish" "$HOME/.config/fish"
+rm -rf "$USER_HOME/.config/i3"
+ln -sf "$USER_HOME/scripts/i3" "$USER_HOME/.config/i3"
 
-rm -rf "$HOME/.config/keepassxc"
-ln -sf "$HOME/scripts/keepassxc" "$HOME/.config/keepassxc"
+rm -rf "$USER_HOME/.config/mpv"
+ln -sf "$USER_HOME/scripts/mpv" "$USER_HOME/.config/mpv"
 
-rm -rf "$HOME/.config/kitty"
-ln -sf "$HOME/scripts/kitty" "$HOME/.config/kitty"
+rm -rf "$USER_HOME/.config/fish"
+ln -sf "$USER_HOME/scripts/fish" "$USER_HOME/.config/fish"
 
-rm -rf "$HOME/.config/sublime-text/Packages/User"
-mkdir -p "$HOME/.config/sublime-text/Packages"
-ln -sf "$HOME/scripts/sublime/User" "$HOME/.config/sublime-text/Packages/User"
+rm -rf "$USER_HOME/.config/keepassxc"
+ln -sf "$USER_HOME/scripts/keepassxc" "$USER_HOME/.config/keepassxc"
 
-rm -rf "$HOME/.icons"
-ln -sf "$HOME/scripts/.icons" "$HOME/.icons"
+rm -rf "$USER_HOME/.config/kitty"
+ln -sf "$USER_HOME/scripts/kitty" "$USER_HOME/.config/kitty"
 
-rm -rf "$HOME/.nanorc"
-ln -sf "$HOME/scripts/.nanorc" "$HOME/.nanorc"
+rm -rf "$USER_HOME/.config/xfce4/"
+ln -sf "$USER_HOME/scripts/xfce4" "$USER_HOME/.config/xfce4"
+
+rm -rf "$USER_HOME/.config/sublime-text/Packages/User"
+mkdir -p "$USER_HOME/.config/sublime-text/Packages"
+ln -sf "$USER_HOME/scripts/sublime/User" "$USER_HOME/.config/sublime-text/Packages/User"
+
+rm -rf "$USER_HOME/.icons"
+ln -sf "$USER_HOME/scripts/.icons" "$USER_HOME/.icons"
+
+rm -rf "$USER_HOME/.nanorc"
+ln -sf "$USER_HOME/scripts/.nanorc" "$USER_HOME/.nanorc"
+
+rm -rf "$USER_HOME/.gitconfig"
+ln -sf "$USER_HOME/scripts/.gitconfig" "$USER_HOME/.gitconfig"
+
+rm -rf "/etc/pacman.conf"
+ln -sf "$USER_HOME/scripts/pacman.conf" "/etc/pacman.conf"
+
+rm -rf "/etc/pacman-mirrors.conf"
+ln -sf "$USER_HOME/scripts/pacman-mirrors.conf" "/etc/pacman-mirrors.conf"
+
+rm -rf "/etc/pacman.d/mirrorlist"
+ln -sf "$USER_HOME/scripts/pacman.d/mirrorlist" "/etc/pacman.d/mirrorlist"
+
