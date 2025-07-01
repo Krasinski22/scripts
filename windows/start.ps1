f4e1c15f0830369136b6057b35f74233b491c04c
Your Name <you@example.com>
2025-06-30 22:22:03 -0300

    b

--- windows/start.ps1
+++ windows/start.ps1
@@ -1,3 +1,5 @@
 & "C:\Program Files\glzr.io\GlazeWM\glazewm.exe"
 & "C:\Users\user\AppData\Local\FlowLauncher\Flow.Launcher.exe"
 & "C:\Users\user\AppData\Local\Microsoft\WinGet\Links\syncthingtray.exe"
+& "C:\Program Files\Everything\Everything.exe" -startup
+& "C:\Program Files\Cloudflare\Cloudflare WARP\Cloudflare WARP.exe"
