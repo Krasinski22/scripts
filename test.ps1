dd82cd1373dbc58cd468402558c6aedb5875198c
Your Name <you@example.com>
2025-07-02 14:25:24 -0300

    b

---
+++ test.ps1
@@ -0,0 +1,3 @@
+# Conteúdo do seu test.ps1
+Write-Host "Funcionou!"
+Read-Host -Prompt "Pressione Enter para fechar..."
--- windows/start.ps1
+++ windows/start.ps1
@@ -1,6 +1,5 @@
-& "C:\Program Files\glzr.io\GlazeWM\glazewm.exe"
+# & "C:\Program Files\glzr.io\GlazeWM\glazewm.exe"
 & "C:\Users\user\AppData\Local\FlowLauncher\Flow.Launcher.exe"
 & "C:\Users\user\AppData\Local\Microsoft\WinGet\Links\syncthingtray.exe"
 & "C:\Program Files\Everything\Everything.exe" -startup
 & "C:\Program Files\Cloudflare\Cloudflare WARP\Cloudflare WARP.exe"
-& "C:\Program Files\WindowsApps\40459File-New-Project.EarTrumpet_2.3.0.0_x86__725pr5jq8wr8a\EarTrumpet\EarTrumpet.exe"
