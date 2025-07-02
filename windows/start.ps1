507252652d0a88653df5a8564c64043f7ebe830c
Your Name <you@example.com>
2025-07-02 11:17:46 -0300

    b

--- windows/start.ps1
+++ windows/start.ps1
@@ -3,3 +3,4 @@
 & "C:\Users\user\AppData\Local\Microsoft\WinGet\Links\syncthingtray.exe"
 & "C:\Program Files\Everything\Everything.exe" -startup
 & "C:\Program Files\Cloudflare\Cloudflare WARP\Cloudflare WARP.exe"
+& "C:\Program Files\WindowsApps\40459File-New-Project.EarTrumpet_2.3.0.0_x86__725pr5jq8wr8a\EarTrumpet\EarTrumpet.exe"
